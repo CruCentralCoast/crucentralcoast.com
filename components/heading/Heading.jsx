1b0c45b49e59042fa9ebd10e516b532bd5820279
Ryan Untalan <untalan.ryan@gmail.com>
2019-11-17 17:08:46 -0800

    added social media icons

--- components/heading/Heading.jsx
+++ components/heading/Heading.jsx
@@ -1,28 +1,26 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
+import * as React from "react";
+import PropTypes from "prop-types";
 // import logo from '../../static/cru_logo.png';
-import { withStyles } from '@material-ui/core/styles';
-import {
-  AppBar,
-  Toolbar,
-} from '@material-ui/core';
+import { withStyles } from "@material-ui/core/styles";
+import { AppBar, Toolbar } from "@material-ui/core";
+import { Facebook, Instagram, Twitter, YouTube } from "@material-ui/icons/";
 
 const drawerWidth = 155;
 
 const styles = style => ({
   header: {
-    textAlign: 'center',
-    width: '100%',
+    textAlign: "center",
+    width: "100%"
   },
 
   headerToolbar: {
-    paddingLeft: '200px',
-    paddingRight: '200px',
+    paddingLeft: "20px",
+    paddingRight: "200px"
   },
 
   headerMenu: {
-    paddingLeft: '200px',
-    paddingRight: '200px',
+    paddingLeft: "200px",
+    paddingRight: "200px"
   },
 
   // logo: {
@@ -30,50 +28,97 @@ const styles = style => ({
   // },
 
   headerLogo: {
-    float: 'left',
-    height: '30px',
-    cursor: 'pointer',
+    float: "left",
+    position: "fixed",
+    top: "-2px",
+    margin: "20px",
+    height: "67px",
+    cursor: "pointer"
   },
 
   headerBody: {
-    height: '60px',
+    height: "60px"
   },
 
   title: {
-    color: '#666062',
-    marginLeft: 'auto',
-    marginRight: 'auto',
+    color: "#666062",
+    marginLeft: "auto",
+    marginRight: "auto"
+  },
+
+  headerSocial: {
+    display: "flex",
+    flexDirection: "row-reverse",
+    marginRight: "40px",
+    marginTop: "20px"
+  },
+
+  socialIcons: {
+    marginLeft: "4px",
+    marginRight: "4px",
+    color: "#d1d3d4",
+    fontSize: "27px"
+  },
+
+  backToCru: {
+    margin: "5px",
+    fontSize: "11px",
+    textDecoration: "none",
+    color: "#666062"
   },
 
   menuToolbar: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: '0 8px',
-    ...style.mixins.toolbar,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: "0 8px",
+    ...style.mixins.toolbar
   },
 
   content: {
     flexGrow: 1,
     width: `calc(100% - ${drawerWidth}px)`,
     marginLeft: drawerWidth,
-    padding: '20px',
-  },
+    padding: "20px"
+  }
 });
 
-const Heading = (props) => {
+const Heading = props => {
   const { classes, children } = props;
 
   return (
     <div className={classes.header}>
-      <AppBar
-        color="default"
-        position="fixed"
-        className={classes.appBar}
-      >
+      <AppBar color="default" position="fixed" className={classes.appBar}>
+        <div className={classes.headerSocial}>
+          <a href="https://www.youtube.com/user/slocrusade" target="_blank">
+            <YouTube className={classes.socialIcons}></YouTube>
+          </a>
+
+          <a href="https://twitter.com/CruCalPolySLO" target="_blank">
+            <Twitter className={classes.socialIcons}></Twitter>
+          </a>
+
+          <a href="https://www.instagram.com/crucalpolyslo/" target="_blank">
+            <Instagram className={classes.socialIcons}></Instagram>
+          </a>
+
+          <a href="https://www.facebook.com/CruCalPolySLO/" target="_blank">
+            <Facebook className={classes.socialIcons}></Facebook>
+          </a>
+
+          <a className={classes.backToCru} href="/">
+            &#8592; back to Cru Central Coast
+          </a>
+        </div>
         <Toolbar className={classes.headerToolbar}>
           <div className={classes.logo}>
-            <a href="/"><img src="/static/cru_logo.png" className={classes.headerLogo} alt="logo" /></a>
+            <a href="/">
+              <img
+                src="/static/cru_logo.png"
+                className={classes.headerLogo}
+                alt="logo"
+              />
+            </a>
           </div>
         </Toolbar>
       </AppBar>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,13 @@
-import * as React from 'react';
+import * as React from "react";
 
-export default (props) => {
+export default props => {
   return (
     <div>
       <img src="/img/landing/cru-central-coast.png" id="CCC" />
 
       <p className="tagline">
-        A family of ministries giving every student and faculty on California's Central Coast the
-        opportunity to hear the Good News of Jesus Christ
+        A family of ministries giving every student and faculty on California's
+        Central Coast the opportunity to hear the Good News of Jesus Christ
       </p>
       <div className="holder">
         <a href="/slo">
@@ -46,100 +46,67 @@ export default (props) => {
         <img src="/img/landing/choose.png" className="ministry" id="choose" />
       </div>
       <style jsx>{`
-      /*
+        /*
         Created on : Jan 24, 2015, 9:07:07 PM
         Author     : kylereis
       */
 
-      @font-face {
-        font-family: "FreightSansProLight";
-        src: url('../fonts/FreigSanProLig.otf') format('opentype');
-      }
+        @font-face {
+          font-family: "FreightSansProLight";
+          src: url("../fonts/FreigSanProLig.otf") format("opentype");
+        }
 
-      @media (min-width: 1300px) {
-        .holder {
-          width: 1212px;
+        @media (min-width: 1300px) {
+          .holder {
+            width: 1212px;
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+          }
+        }
+
+        @media (min-width: 1000px) and (max-width: 1299px) {
+          .holder {
+            width: 912px;
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+          }
+        }
+
+        @media (min-width: 700px) and (max-width: 999px) {
+          .holder {
+            width: 612px;
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+          }
+        }
+
+        @media (max-width: 699px) {
+          .holder {
+            width: 300px;
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+          }
+        }
+
+        #CCC {
+          height: 200px;
           display: block;
+          margin-top: 10pt;
           margin-left: auto;
           margin-right: auto;
         }
-      }
 
-      @media (min-width: 1000px) and (max-width: 1299px){
-        .holder {
-          width: 912px;
-          display: block;
-          margin-left: auto;
-          margin-right: auto;
-        }
-      }
-
-      @media (min-width: 700px) and (max-width: 999px){
-        .holder {
-          width: 612px;
-          display: block;
-          margin-left: auto;
-          margin-right: auto;
-        }
-      }
-
-      @media (max-width: 699px) {
-        .holder {
-          width: 300px;
-          display: block;
-          margin-left: auto;
-          margin-right: auto;
-        }
-      }
-
-      #CCC {
-        height: 200px;
-        display: block;
-        margin-top: 10pt;
-        margin-left: auto;
-        margin-right: auto;
-      }
-
-      p.tagline {
-        padding-top: 7pt;
-        padding-bottom: 7pt;
-        margin-top: 10pt;
-        margin-bottom: 20pt;
-        margin-right: 8%;
-        margin-left: 8%;
-        text-align: center;
-        font-family: "FreightSansProLight";
-        font-size: 14pt;
-        color: #ffffff;
-        border-top-color: #ffffff;
-        border-top-width: 1pt;
-        border-top-style: solid;
-        border-bottom-color: #ffffff;
-        border-bottom-width: 1pt;
-        border-bottom-style: solid;
-      }
-
-      .ministry {
-        /* display: table; */
-        margin-bottom: 20pt;
-        /* margin-left: auto; */
-        /* margin-right: auto; */
-      }
-
-      #choose {
-        margin-left: 40pt;
-        /* width: 280px; */
-      }
-
-      @media (min-width: 1440px) {
         p.tagline {
-          width: 1200px;
           padding-top: 7pt;
           padding-bottom: 7pt;
           margin-top: 10pt;
           margin-bottom: 20pt;
-          margin-right: auto;
-          margin-left: auto;
+          margin-right: 8%;
+          margin-left: 8%;
           text-align: center;
           font-family: "FreightSansProLight";
           font-size: 14pt;
@@ -152,33 +119,66 @@ export default (props) => {
           border-bottom-style: solid;
         }
 
-        .holder {
-          width: 1230px;
-          display: block;
-          margin-left: auto;
-          margin-right: auto;
+        .ministry {
+          /* display: table; */
+          margin-bottom: 20pt;
+          /* margin-left: auto; */
+          /* margin-right: auto; */
         }
-      }
-    `}</style>
-    <style jsx global>{`
-      body {
-        background-color: #959595;
-      }
-      @media (min-width: 1300px) {
+
+        #choose {
+          margin-left: 40pt;
+          /* width: 280px; */
+        }
+
+        @media (min-width: 1440px) {
+          p.tagline {
+            width: 1200px;
+            padding-top: 7pt;
+            padding-bottom: 7pt;
+            margin-top: 10pt;
+            margin-bottom: 20pt;
+            margin-right: auto;
+            margin-left: auto;
+            text-align: center;
+            font-family: "FreightSansProLight";
+            font-size: 14pt;
+            color: #ffffff;
+            border-top-color: #ffffff;
+            border-top-width: 1pt;
+            border-top-style: solid;
+            border-bottom-color: #ffffff;
+            border-bottom-width: 1pt;
+            border-bottom-style: solid;
+          }
+
+          .holder {
+            width: 1230px;
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+          }
+        }
+      `}</style>
+      <style jsx global>{`
         body {
-          background-image: url("../img/landing/Bishop-peak_gray.jpg");
-          background-repeat: no-repeat;
-          background-size: 100%;
-          background-origin: content-box;
+          background-color: #959595;
         }
-      }
-      @media (min-width: 700px) {
-        body {
-          background-image: url("../img/landing/Bishop-peak_gray.jpg");
-          background-repeat: no-repeat;
+        @media (min-width: 1300px) {
+          body {
+            background-image: url("../img/landing/Bishop-peak_gray.jpg");
+            background-repeat: no-repeat;
+            background-size: 100%;
+            background-origin: content-box;
+          }
         }
-      }
-    `}</style>
+        @media (min-width: 700px) {
+          body {
+            background-image: url("../img/landing/Bishop-peak_gray.jpg");
+            background-repeat: no-repeat;
+          }
+        }
+      `}</style>
     </div>
   );
 };
