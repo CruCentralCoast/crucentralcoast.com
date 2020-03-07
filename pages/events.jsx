898089c5ba94d0463117577f45237c52b98799a9
fedorareis <fedorareis@gmail.com>
2020-03-07 14:00:39 -0800

    disabled events

--- pages/events.jsx
+++ pages/events.jsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import Layout from '../components/MyLayout';
-import Events from '../components/events/Events';
+// import Events from '../components/events/Events';
 
 
 export default () => (
   <Layout>
-    <Events />
+    {/* <Events /> */}
   </Layout>
 );
